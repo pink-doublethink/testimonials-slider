a487eb8195ffb26742abbfa264c73a43ccc89d60
Alexey Ovsyannikov <soliterco@gmail.com>
2022-12-13 15:40:08 +0300

    improved description and structure of the project

--- README.md
+++ README.md
@@ -2,8 +2,15 @@
 ----
 ### What is the project about?
 ---
+- The project was created as part of communication with Frontend Mentor community. 
 - Having started learning front-end development in 2021, I have hardly had to deal with layouts built around floats. Deciding to fix this, I wrote this project to try floats technology. 
 - As a result of the completion of the project, I did not know this technology better, but I remain faithful to grids.
+---
+### Links
+---
+- [Link](https://elegant-dieffenbachia-344afb.netlify.app/) to Live Demo;
+
+- [Link](https://www.frontendmentor.io/challenges/coding-bootcamp-testimonials-slider-4FNyLA8JL) to the start page of the task;
 ---
 ### How to install and run a project
 ---
--- src/App.tsx
+++ src/App.tsx
@@ -9,28 +9,28 @@ import './App.css'
 const data = [
   {
       id: 1,
-      image: '/src/assets/images/image-john.jpg',
+      image: "/image-john.jpg",
       name: "John Tarkpor",
       job: "Junior Front-end Developer",
       description: '“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”'
   },
   {
       id: 2,
-      image: '/src/assets/images/image-tanya.jpg',
+      image: "/image-tanya.jpg",
       name: "Tanya Sinclair",
       job: "UX Engineer",
       description: '“I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future.”'
   },
   {
       id: 3,
-      image: '/src/assets/images/image-john.jpg',
+      image: "/image-john.jpg",
       name: "John Tarkpor",
       job: "Junior Front-end Developer",
       description: '“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”'
   },
   {
       id: 4,
-      image: '/src/assets/images/image-tanya.jpg',
+      image: "/image-tanya.jpg",
       name: "Tanya Sinclair",
       job: "UX Engineer",
       description: '“I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future.”'
